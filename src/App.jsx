ccc6831753aa97f3a6e69f7b6f1b5fe0ee5ad277
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-07 16:17:34 +0300

    giriş

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-  let deneme = 5;
-  
+
+  let a = 15;
   // üst tarafada  javascript kodlarımızı yazıyoruz 
+  // bu kodları return içerisinde gösterebilmek için yapmamız gereken şekilli parntezler içinde yazmaktır 
+  const firstName = "Emre";
+  let vize = 60;
+  let vize2 = 80;
+  let sonuc = true;
+  let isimler =  ["emre","yaman","ahmet","mehmet"]
   return (
-    <div>Buraya return içinde  Html kodlarını yazıyoruz </div>
+    <div>
+      <p>a değişkenin değeri : {a}</p>
+      <p>Yazılımcının Adı  : {firstName}</p>
+      <p>ortalama : {(vize + vize2) / 2}</p>
+      {sonuc ? <p>Ehliyeti alabilirsin</p>: <p>Ehliteti alamazsın</p>}
+
+      {/* ekrana yukarı sonuca true verdiğimiz için kısa if ile ehilyeti alabilrisin yazar  */}
+
+      {
+        (vize+vize2) /2 > 50 ? <p>Dersten Geçtin</p> : <p> Dersten kaldın </p>
+      }
+
+      {isimler.map((isim,index) => (
+      <div style={{ color : "red" }} key={index}>{isim}</div>
+      ))
+      }
+
+
+    </div>
   )
 }
 
