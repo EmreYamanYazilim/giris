9147893f52d66c7d5868ca7a4b80f4e4df3637d6
EmreYamanYazilim <emreyamanyazilim@gmail.com>
2024-11-07 11:28:52 +0300

    ilk commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let deneme = 5;
+  
+  // üst tarafada  javascript kodlarımızı yazıyoruz 
+  return (
+    <div>Buraya return içinde  Html kodlarını yazıyoruz </div>
+  )
+}
+
+export default App
